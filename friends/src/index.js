b7756276338c9ee68d4f2fe7109c04155999b436
NathanWaddell121107 <waddellnathan1211@gmail.com>
2019-04-18 20:26:09 -0500

    set up index.js and friends reducer.

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './reducers/index';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
